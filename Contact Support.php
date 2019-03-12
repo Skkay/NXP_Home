7a5025d973fe74e3e21f3710ac8812cc5e0be172
aaronbul <aaron.baf@gmail.com>
2019-03-12 13:33:14 +0100

    contact ajout

---
+++ Contact Support.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <link rel="icon" href="../../favicon.ico">
+  <link href="bootstrap-3.3.7/dist/css/bootstrap.css" rel="stylesheet">
+  <link href="navbar-fixed-top.css" rel="stylesheet">
+
+  <title>Contact</title>
+
+
+</head>
+
+
+<body>
+  <?php include("fix/navigation_bar.php"); ?>
+
+  <div class="container">
+
+    <div class="jumbotron">
+
+      <h1 id="JQcolor">Contactez-nous</h1>
+      <p>Médiathèque en ligne</p>
+
+      <p style = "color :#00b386">Nos équipes Support sont disponibles de 9h à 18h du lundi au vendredi.</p>
+      <!--
+      <p><strong>Support technique : </strong><p>
+        <p style = "line-height: 9px">Téléphone : 06.42.68.16.68</p>
+        <p style = "line-height: 9px">email : 
+          <a href="mailto:thomas.bonneau64@gmail.com">thomas.bonneau64@gmail.com</a>
+        </p>
+
+        <p><strong>Base de donnée : </strong><p>
+          <p style = "line-height: 9px">Téléphone : 06.12.29.02.55</p>
+          <p style = "line-height: 9px">email : 
+            <a href="mailto:emilie.parison@gmail.com">emilie.parison@gmail.com</a>
+          </p>
+
+          <p><strong>Divers : </strong><p>
+            <p style = "line-height: 9px">Téléphone : 07.66.12.00.53</p>
+            <p style = "line-height: 9px">email : 
+              <a href="mailto:aaron.baf@gmail.com">aaron.baf@gmail.com</a>
+            </p>
+            <a href =" https://www.google.fr/maps/place/Lycee+sainte+marguerite/@47.3461495,0.7194182,17z/data=!4m5!3m4!1s0x47fcd6fa5eb820c1:0x4fa67fadf78edd5a!8m2!3d47.3462113!4d0.7213172">
+              <img src = "map.png" style="width:300px;height:250px;"/>
+            </a>
+          -->
+
+          <p>
+            <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Support technique</a>
+            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Support BDD</button>
+            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3">Support Client</button>
+            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample4" aria-expanded="false" aria-controls="multiCollapseExample4">Map</button>
+            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2 multiCollapseExample3 multiCollapseExample4">All support</button>
+          </p>
+
+          <div class="row">
+            <div class="col">
+              <div class="collapse multi-collapse" id="multiCollapseExample1">
+                <div class="card card-body">
+                  <p><strong>Support technique : </strong><p>
+                    <p style = "line-height: 9px">Téléphone : 06.42.68.16.68<</p>
+                    <p style = "line-height: 9px">email : 
+                      <a href="mailto:thomas.bonneau64@gmail.com">thomas.bonneau64@gmail.com</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="collapse multi-collapse" id="multiCollapseExample2">
+                  <div class="card card-body">
+                    <p><strong>Base de donnée : </strong><p>
+                      <p style = "line-height: 9px">Téléphone : 06.12.29.02.55</p>
+                      <p style = "line-height: 9px">email : 
+                        <a href="mailto:emilie.parison@gmail.com">emilie.parison@gmail.com</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="collapse multi-collapse" id="multiCollapseExample3">
+                    <div class="card card-body">
+                      <p><strong>Divers : </strong><p>
+                        <p style = "line-height: 9px">Téléphone : 07.66.12.00.53</p>
+                        <p style = "line-height: 9px">email : 
+                          <a href="mailto:aaron.baf@gmail.com">aaron.baf@gmail.com</a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample4">
+                      <div class="card card-body">
+                        <p><strong>Siège social </strong><p>
+                          <p style = "line-height: 9px">2 rue, Horizon vert</p>
+                          <p style = "line-height: 9px">37170 Chambray-lès-Tours</p>
+                          <a href =" https://www.google.fr/maps/place/Lycee+sainte+marguerite/@47.3461495,0.7194182,17z/data=!4m5!3m4!1s0x47fcd6fa5eb820c1:0x4fa67fadf78edd5a!8m2!3d47.3462113!4d0.7213172">
+                            <img src = "map.png" style="width:300px;height:250px;"/>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+            <script src="bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
+
+          </body>
+          </html>
