21ae00f404c5a90f4aec95888d1692de2ea6c2ac
Skkay <skkay.thomas@gmail.com>
2019-03-16 21:14:52 +0100

    Mise en place du nouveau formulaire d'inscription
    "Inscription v2" et "inscription v2_finale" en tant que formulaire d’inscription par défaut.

--- inscription.php
+++ inscription.php
@@ -1,71 +1,129 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
 
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <link rel="icon" href="../../favicon.ico">
-    <link href="bootstrap-3.3.7/dist/css/bootstrap.css" rel="stylesheet">
-    <link href="navbar-fixed-top.css" rel="stylesheet">
-    <link href="style_form.css" rel="stylesheet">
+  <link rel="icon" href="../../favicon.ico">
+  <link href="bootstrap-3.3.7/dist/css/bootstrap.css" rel="stylesheet">
+  <!-- <link href="navbar-fixed-top.css" rel="stylesheet"> -->
+  <link href="style_form.css" rel="stylesheet">
 
-    <title>Inscription - NXP Home</title>
-
-
-  </head>
+  <title>Inscription - NXP Home</title>
 
 
-  <body>
+</head>
 
-    <?php include("fix/navigation_bar.php"); ?>
 
-    <div class="container">
+<body>
 
-      <div class="jumbotron">
-        <h1 id="JQcolor">Inscription</h1>
-        <p>Médiathèque en ligne</p>
+  <?php include("fix/navigation_bar.php"); ?>
 
-        <form method="post" action="inscription_final.php">
+  <div class="container">
 
-          <div>
-            <label>Pseudo : </label>
-            <input type="text" name="pseudo"/>
-            <p></p>
+    <div class="jumbotron">
+      <h1 id="JQcolor">Inscription</h1>
+      <p>Médiathèque en ligne</p>
+      <hr class="my-4">
+
+      <!-- DEBUT FORMULAIRE INSCRIPTION -->
+      <form method="post" action="inscription_final.php">
+
+        <!-- Identifiant / Email -->
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Identifiant</label>
+            <input type="text" class="form-control" placeholder="Identifiant" name="pseudo">
+          </div>
+          <div class="form-group col-md-6">
+            <label>Email</label>
+            <input type="email" class="form-control" placeholder="Email" name="email">
+          </div>
+        </div>
+
+        <!-- Mot de passe / Confirmation mot de passe -->
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Mot de passe</label>
+            <input type="password" class="form-control" placeholder="Mot de passe" name="password">
           </div>
 
-          <div>
-            <label>Mot de passe : </label>
-            <input type="password" name="password"/>
-            <p></p>
+          <div class="form-group col-md-6">
+            <label>Confirmer le mot de passe</label>
+            <input type="password" class="form-control" placeholder="Confirmer le mot de passe" name="confirm_password">
+          </div>
+        </div>
+
+        <!-- Nom / Prenom / Date de naissance / Civilité -->
+        <div class="form-row">
+          <div class="form-group col-md-3">
+            <label>Prénom</label>
+            <input type="text" class="form-control" placeholder="Prénom" name="prenom">
           </div>
 
-          <div>
-            <label>Confirmer le mot de passe : </label>
-            <input type="password" name="confirm_password"/>
-            <p></p>
+          <div class="form-group col-md-3">
+            <label>Nom</label>
+            <input type="text" class="form-control" placeholder="Nom" name="nom">
           </div>
 
-          <div>
-            <label>Email : </label>
-            <input type="text" name="email"/>
-            <p></p>
+          <div class="form-group col-md-3">
+            <label>Date de naissance</label>
+            <input type="date" class="form-control" placeholder="Date de naissance" name="ddn">
           </div>
 
-          <input type="submit" value="Valider"/>
-
-        </form>
+          <div class="form-group col-md-3">
+            <!--
+            <div class="custom-control custom-radio custom-control-inline">
+              <input type="radio" class="custom-control-input" name="customRadioInline1">
+              <label>1</label>
+            </div>
+            <div class="custom-control custom-radio custom-control-inline">
+              <input type="radio" class="custom-control-input" name="customRadioInline1">
+              <label>2</label>
+            </div>
+          -->
+          <label>Civilité</label>
+          <select class="form-control" name="civilite">
+            <option value="ND" selected="">Non-définie</option>
+            <option value="Homme">Homme</option>
+            <option value="Femme">Femme</option>
+          </select>
+        </div>
 
       </div>
 
-    </div>
+      <!-- Adresse / Ville / Code postal -->
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label>Adresse</label>
+          <input type="text" class="form-control" placeholder="Adresse" name="adresse">
+        </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
+        <div class="form-group col-md-4">
+          <label>Ville</label>
+          <input type="text" class="form-control" placeholder="Ville" name="ville">
+        </div>
 
-  </body>
+        <div class="form-group col-md-2">
+          <label>Code postal</label>
+          <input type="text" class="form-control" placeholder="Code postal" name="code_postal">
+        </div>      
+      </div>
+
+      <button type="submit" class="btn btn-primary btn-lg btn-block">Valider</button>
+
+    </form>
+
+  </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
+
+</body>
 </html>
