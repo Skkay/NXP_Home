194b381465b21b81878247d402368cbc18e80976
Skkay <skkay.thomas@gmail.com>
2019-04-10 00:10:31 +0200

    Ajout de index.php dans /media/

---
+++ media/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" href="../../favicon.ico">
+    <link href="../bootstrap-3.3.7/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="../navbar-fixed-top.css" rel="stylesheet">
+
+    <title>Media - NXP Home</title>
+
+
+  </head>
+
+
+  <body>
+    <?php include("../static/navigation_bar.php"); ?>
+
+    <div class="container">
+
+      <div class="jumbotron">
+
+        <h1 id="JQcolor">NXP Home</h1>
+        <p>Médiathèque en ligne</p>
+        <hr class="my-4">
+        <p></p>
+
+      </div>
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="../bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
